a8de2ae1aa68bd96fdd68807b553ec8831c109de
soylentOrange <soylentOrange@users.noreply.github.com>
2026-08-23 21:42:48 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .parse()
         .unwrap();
     let slave_id: u16 = env::var("FRONIUS_MODBUS_SLAVE_ID")
-        .unwrap_or_else(|_| "240".to_string())
+        .unwrap_or_else(|_| "126".to_string())
         .parse()
         .expect("Invalid SLAVE_ID");
     println!("Starting Fronius modbus bridge on: {socket_addr} - Slave-ID: {slave_id}");
