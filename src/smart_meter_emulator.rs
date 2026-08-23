b30014cda718f10b30157f9847710bd084537a6b
soylentOrange <soylentOrange@users.noreply.github.com>
2026-08-23 21:04:05 +0200

    Update smart_meter_emulator.rs

--- src/smart_meter_emulator.rs
+++ src/smart_meter_emulator.rs
@@ -76,7 +76,7 @@ impl tokio_modbus::server::Service for SmartMeterEmulator {
 }
 
 impl SmartMeterEmulator {
-    pub fn new(slave) -> (Self, Sender<Readings>) {
+    pub fn new(slave:u8) -> (Self, Sender<Readings>) {
         let id = slave;
         // Insert some test data as register values.
         let mut input_registers = HashMap::new();
